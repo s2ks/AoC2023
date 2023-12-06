890f8ab932892f733f37e653d9bd9216fc6a8ba3
Jilles Bakker <j.j.bakker6@students.uu.nl>
2023-12-06 16:58:26 +0100

    tidy

--- day6/Main.hs
+++ day6/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Util
 
-
 -- >>> product $ countWays [7, 15, 30] [9, 40, 200] []
 -- 288
 
@@ -10,21 +9,18 @@ countWays :: [Int] -> [Int] -> [Int] -> [Int]
 countWays [] [] acc = acc
 countWays (t:times) (d:distances) acc =
    countWays times distances $ (length . filter (> d) . map (\hold -> (t - hold) * hold)) [1..t] : acc
-  
 
 main :: IO ()
 main = do
   lines <- input
 
+  -- Part 1
   let times = (map read . words . drop 1 . dropWhile (/= ':') . head) lines
   let distances = (map read . words . drop 1 . dropWhile (/= ':') . last) lines
-
   let ways = countWays times distances []
-
   print $ product ways
 
-
-  let time = (read . concat  . words . drop 1 . dropWhile (/= ':') . head) lines :: Int 
-  let distance = (read. concat . words . drop 1 . dropWhile (/= ':') . last) lines :: Int
-
+  -- Part 2
+  let time = (read . concat . words . drop 1 . dropWhile (/= ':') . head) lines
+  let distance = (read . concat . words . drop 1 . dropWhile (/= ':') . last) lines
   print $ product (countWays [time] [distance] [])
