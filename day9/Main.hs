fbc072c9209cf6efbd3d856f0cf056eebae416b7
Jilles Bakker <j.j.bakker6@students.uu.nl>
2023-12-09 13:58:46 +0100

    remove dependency

--- day9/Main.hs
+++ day9/Main.hs
@@ -1,16 +1,12 @@
 module Main where
 
-import Util
-
 diffs :: [Int] -> [Int]
 diffs (x:y:xs) = y-x:diffs (y:xs)
 diffs _ = []
 
 main :: IO ()
 main = do
-  lines <- input
-
-  let parsed = map (map read . words) lines
+  parsed <- map (map read . words) . lines <$> getContents
   let dsss = map (\xs -> until (all (== 0) . head) (\xss -> (diffs . head) xss : xss) [diffs xs, xs]) parsed
 
   -- part 1
