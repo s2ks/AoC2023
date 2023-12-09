dab8f19621ec689142726191778a705f8f9a41ae
Jilles Bakker <j.j.bakker6@students.uu.nl>
2023-12-09 13:22:34 +0100

    day 9

---
+++ day9/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Util
+
+diffs :: [Int] -> [Int]
+diffs (x:y:xs) = y-x:diffs (y:xs)
+diffs _ = []
+
+main :: IO ()
+main = do
+  lines <- input
+
+  let parsed = map (map read . words) lines :: [[Int]]
+  let dsss = map (\xs -> until (all (== 0) . head) (\xss -> (diffs . head) xss : xss) [diffs xs, xs]) parsed
+
+  -- part 1
+  let adds = map (foldl (\a ds -> last ds + a) 0) dsss
+  print $ sum adds
+
+  -- part 2
+  let pres = map (foldl (\a ds -> head ds - a) 0) dsss
+  print $ sum pres
